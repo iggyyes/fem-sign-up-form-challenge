7545866a7cbcfb5722d001b1794f5a34a7fa98ca
iggyyes <igamwisniewska@gmail.com>
2023-03-25 15:19:48 +0000

    html css banner refactor

--- app/js/script.js
+++ app/js/script.js
@@ -1,1 +1,58 @@
-console.log('pieczareczki')
+const form = document.querySelector('form')
+const email = document.getElementById('email')
+
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault()
+
+    checkInputs()
+})
+
+function checkInputs() {
+    // get values from the inputs
+    const emailValue = email.value.trim()
+    
+
+    
+
+    if(emailValue === '') {
+		setErrorFor(email, 'Email cannot be blank');
+	} else if (!isEmail(emailValue)) {
+        setErrorFor(email, 'Looks like this is not an email');
+    } else {
+        setSuccessFor(email)
+        
+    }
+
+    
+}
+
+function setErrorFor(input, message) {
+    const signUpField = input.parentElement  //.signUp__field
+    const error = document.getElementById('errorMessage')
+
+    //add error message inside p
+    error.innerHTML = message
+
+    // add error class
+    email.setAttribute("aria-invalid", "true")
+
+}
+
+function setSuccessFor(input) {
+    const signUpField = input.parentElement  //.signUp__field
+    const error = document.getElementById('errorMessage')
+
+    //add error message inside p
+    error.innerHTML = ''
+
+    // add success class
+    email.setAttribute("aria-invalid", "false")
+
+}
+
+
+
+function isEmail(email){
+    return /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email);
+}
--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,6 @@
 // Initialize modules
 const { src, dest, watch, series } = require('gulp');
-const sass = require('gulp-sass')(require('sass'));
+const sass = require('gulp-sass')(require('dart-sass'));
 const postcss = require('gulp-postcss');
 const autoprefixer = require('autoprefixer');
 const cssnano = require('cssnano');
@@ -9,7 +9,7 @@ const terser = require('gulp-terser');
 const browsersync = require('browser-sync').create();
 
 // Use dart-sass for @use
-sass.compiler = require('sass');
+sass.compiler = require('dart-sass');
 
 // Sass Task
 function scssTask() {

85c7fb5a41c9e8e3665af9c2b6250b326c7d0e7a
iggyyes <igamwisniewska@gmail.com>
2023-03-25 20:50:03 +0000

    js form validation add

--- app/js/script.js
+++ app/js/script.js
@@ -1,5 +1,9 @@
 const form = document.querySelector('form')
+const firstName = document.getElementById('firstName')
+const lastName = document.getElementById('lastName')
 const email = document.getElementById('email')
+const password = document.getElementById('password')
+
 
 
 form.addEventListener('submit', (e) => {
@@ -10,47 +14,68 @@ form.addEventListener('submit', (e) => {
 
 function checkInputs() {
     // get values from the inputs
+    const firstNameValue = firstName.value.trim()
+    const lastNameValue = lastName.value.trim()
     const emailValue = email.value.trim()
+    const passwordValue = password.value.trim()
     
 
-    
+    if(firstNameValue === '') {
+        setErrorFor(firstName, 'First Name cannot be empty')
+    } else {
+        setSuccessFor(firstName)
+    }
+
+    if(lastNameValue === '') {
+        setErrorFor(lastName, 'Last Name cannot be empty');
+    } else {
+        setSuccessFor(lastName);
+    }
 
     if(emailValue === '') {
-		setErrorFor(email, 'Email cannot be blank');
+		setErrorFor(email, 'Email cannot be empty');
 	} else if (!isEmail(emailValue)) {
         setErrorFor(email, 'Looks like this is not an email');
     } else {
         setSuccessFor(email)
-        
     }
 
-    
+    if(passwordValue === '') {
+        setErrorFor(password, 'Password cannot be empty')
+    } else if (passwordValue.length <= 8) {
+        setErrorFor(password, 'Password must be minimum 8 characters long')
+    } else if (passwordValue.length >= 20) {
+        setErrorFor(password, 'Password must be maximum 20 characters long')
+    } else {
+        setSuccessFor(password)
+    }
+
 }
+
 
 function setErrorFor(input, message) {
     const signUpField = input.parentElement  //.signUp__field
-    const error = document.getElementById('errorMessage')
+    const error = signUpField.querySelector('#errorMessage')
 
     //add error message inside p
     error.innerHTML = message
 
     // add error class
-    email.setAttribute("aria-invalid", "true")
+    input.setAttribute("aria-invalid", "true")
 
 }
 
 function setSuccessFor(input) {
     const signUpField = input.parentElement  //.signUp__field
-    const error = document.getElementById('errorMessage')
+    const error = signUpField.querySelector('#errorMessage')
 
     //add error message inside p
     error.innerHTML = ''
 
     // add success class
-    email.setAttribute("aria-invalid", "false")
+    input.setAttribute("aria-invalid", "false")
 
 }
-
 
 
 function isEmail(email){
